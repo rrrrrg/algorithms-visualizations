ead8d213b97310b804d9beca8e97afbd505f1941
Ran Guo <rguo@greenactionstudio.com>
2024-02-26 23:45:39 -0500

    minor

--- wasm-libs/src/data_structures/graph.rs
+++ wasm-libs/src/data_structures/graph.rs
@@ -87,8 +87,8 @@ impl Graph {
 
     pub fn get_neighbor_indexes(&self, row: u32, column: u32) -> Vec<usize> {
         let mut indexes = Vec::with_capacity(8);
-        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
-            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+        for delta_row in [0, self.height - 1, 1].iter().cloned() {
+            for delta_col in [0, 1, self.width - 1].iter().cloned() {
                 if delta_row == 0 && delta_col == 0 {
                     continue;
                 }
